ea015166a62dd17af223f6461c6a4d96da8fed36
Mohameddacar <mohameddacarmohumed@gmail.com>
2025-03-27 05:06:46 +0300

    navber responsive

--- src/layout/Navber.jsx
+++ src/layout/Navber.jsx
@@ -1,14 +1,24 @@
 import  Container from '../components/Container'
-import React from 'react'
+import React, { useState } from 'react'
 import Flex from '../components/Flex'
 import Image from '../components/Image'
 import Logo from '../assets/logo.png'
 import Lists from '../components/Lists'
 import Button from '../components/Button'
 import { Link } from 'react-router-dom'
+import { FaBars } from 'react-icons/fa'
+import { ImCross } from 'react-icons/im'
 const Navber = () => {
+
+  let [open,setOpen] = useState(false);
+  let handleClick = ()=>{
+    setOpen(!open)
+
+  }
   return (
-    <section className='py-[18px]'>
+    <>
+    {/* Desktop Design Start */}
+    <nav className='hidden md:block py-[18px]'>
         <Container>
                 <Flex className='items-center'>
                 <div  className='w-3/12'>
@@ -29,7 +39,33 @@ const Navber = () => {
                 </div>
                 </Flex>
         </Container>
-        </section>
+        </nav>
+     {/* Desktop Design ends  */}
+
+
+    {/* Mobile Design Start*/}
+
+    <nav className='relative block md:hidden px-3 py-5'>
+        <Flex className='items-center justify-between'>
+        <div><Image src={Logo}/></div>
+        <div onClick={handleClick}>
+          {
+            open ?<ImCross /> : <FaBars />
+          }</div>
+        </Flex>
+        {
+          open && <ul className='absolute top-[78px] left-0 z-40 w-full bg-blue-500 flex flex-col justify-center items-center py-14 gap-y-12 font-pop font-semibold text-white text-lg'>
+          <li>Home</li>
+          <li>Service</li>
+          <li>About</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+        }
+        </nav>
+
+    {/* Mobile Design ends*/}
+    </>
   )
 }
 
